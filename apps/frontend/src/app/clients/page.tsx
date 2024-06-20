e5102b6b4cb1bbdec123436a956affdd987fa722
Sebastián Cruz <cruzjesussebastian@gmail.com>
2024-06-19 21:11:05 -0300

    feat(frontend/clients): update index page to client component

--- apps/frontend/src/app/clients/page.tsx
+++ apps/frontend/src/app/clients/page.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import {
   ActionIcon,
   Group,
+  Skeleton,
   Table,
   TableTbody,
   TableTd,
@@ -10,12 +13,27 @@ import {
   Title,
   rem
 } from '@mantine/core';
-import { getAllResources } from '../../utils/api';
+import { deleteResource, getAllResources } from '../../utils/api';
 import { IconEye, IconPencil, IconTrash } from '@tabler/icons-react';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-export default async function Page() {
-  const clients = await getAllResources(class Client {});
+export default function Page() {
+  const [clients, setClients] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getClients() {
+      const clients = await getAllResources(class Client {});
+
+      console.log(clients);
+
+      setClients(clients);
+      setLoading(false);
+    }
+
+    getClients();
+  }, []);
 
   return (
     <>
@@ -31,26 +49,61 @@ export default async function Page() {
         </TableThead>
 
         <TableTbody>
-          {clients.map((client) => {
-            return (
-              <TableTr key={client.id}>
-                <TableTd>{client.id}</TableTd>
-                <TableTd>{client.name}</TableTd>
-                <TableTd>{client.email}</TableTd>
-                <TableTd>{client.phone}</TableTd>
-                <TableTd>
-                  <Group gap={0} justify="flex-end">
-                    <ActionIcon variant="subtle" color="gray" component={Link} href={`/clients/${client.id}`}>
-                      <IconEye
-                        style={{ width: rem(16), height: rem(16) }}
-                        stroke={1.5}
-                      />
-                    </ActionIcon>
-                  </Group>
-                </TableTd>
-              </TableTr>
-            );
-          })}
+          {loading && (
+            <>
+            <TableTr>
+              <TableTd colSpan={5}>
+                <Skeleton>Loading...</Skeleton>
+              </TableTd>
+            </TableTr>
+            <TableTr>
+              <TableTd colSpan={5}>
+                <Skeleton>Loading...</Skeleton>
+              </TableTd>
+            </TableTr>
+            <TableTr>
+              <TableTd colSpan={5}>
+                <Skeleton>Loading...</Skeleton>
+              </TableTd>
+            </TableTr>
+            <TableTr>
+              <TableTd colSpan={5}>
+                <Skeleton>Loading...</Skeleton>
+              </TableTd>
+            </TableTr>
+            <TableTr>
+              <TableTd colSpan={5}>
+                <Skeleton>Loading...</Skeleton>
+              </TableTd>
+            </TableTr>
+            </>
+          )}
+          {clients &&
+            clients.map((client) => {
+              return (
+                <TableTr key={client.id}>
+                  <TableTd>{client.id}</TableTd>
+                  <TableTd>{client.name}</TableTd>
+                  <TableTd>{client.email}</TableTd>
+                  <TableTd>{client.phone}</TableTd>
+                  <TableTd>
+                    <Group gap={0} justify="flex-end">
+                      <ActionIcon
+                        variant="subtle"
+                        color="gray"
+                        component={Link}
+                        href={`/clients/${client.id}`}
+                      >
+                        <IconEye
+                          style={{ width: rem(16), height: rem(16) }}
+                          stroke={1.5}
+                        />
+                      </ActionIcon>
+                    </Group>
+                  </TableTd>
+                </TableTr>
+              );
+            })}
         </TableTbody>
       </Table>
     </>
